467ed1906092b5490998aea6f078310d4f8640fc
Mauricio Elian Delgadillo Garcia <mau.elian.dg@gmail.com>
2024-02-07 18:03:41 -0400

    feat: add map

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
 from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
-import pandas as pd
+import dash_mantine_components as dmc
+import dash_leaflet as dl
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
-
-app = Dash(__name__)
-
-app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-])
-
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+app = Dash(
+    __name__, 
+    external_stylesheets=[
+        "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;900&display=swap"
+    ]
 )
 
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+app.layout = dmc.Container(
+    children=[
+        dmc.Title(f"Bioinformatics Postgraduate Degree Finder", order=1, align="center"),
+        dmc.Text("A map for Masters and PhD Programs Information", size="xl", py=10),
+        dl.Map(dl.TileLayer(), center=[32,0], zoom=2, style={'height':'75vh'})
+    ], size="xl"
+)
 
 if __name__ == '__main__':
     app.run(debug=True)
