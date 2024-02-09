d1cb69d30c7eea10fe9914683a2e28652b71bb5c
Mauricio Elian Delgadillo Garcia <mau.elian.dg@gmail.com>
2024-02-08 21:58:53 -0400

    feat: add Select countries

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from dash import Dash, html, dcc, callback, Output, Input
+from countryinfo import CountryInfo
+
 import dash_mantine_components as dmc
 import dash_leaflet as dl
 import pandas as pd
+import json
 
+# Leer el archivo JSON
+with open('countries.json', 'r') as file:
+    data = json.load(file)
+
+# Acceder a la lista de países
+countries = data['countries_names']
+country = "United States"
+geojson_data = CountryInfo(country).geo_json()
+
+# Acceder a la tabla de Datos de los programas
 df = pd.read_excel('data.xlsx')
 
 def popUpText(index):
@@ -26,8 +39,20 @@ app.layout = dmc.Container(
     children=[
         dmc.Title(f"Bioinformatics Postgraduate Degree Finder", order=1, align="center"),
         dmc.Text("A map for Masters and PhD Programs Information", size="xl", py=10),
+        html.Div([
+            dmc.Select(
+                label="Select Country",
+                placeholder="Select one",
+                id="country-select",
+                value=country,
+                zIndex=1001,
+                style={"marginBottom": 16},
+                data=[{"value": countryName, "label": countryName} for countryName in countries]
+            )
+        ]),
         dl.Map([
             dl.TileLayer(),
+            dl.GeoJSON(data=geojson_data, zoomToBounds=True, id='geojson'),
             *[dl.Marker(
                 title=df['University'][ind],
                 position={"lat": df['Latitude'][ind], "lng": df['Longitude'][ind]},
@@ -36,6 +61,12 @@ app.layout = dmc.Container(
         ], center=[32,0], zoom=2, style={'height':'75vh'})
     ], size="xl"
 )
+
+@callback(Output("geojson", "data"), Input("country-select", "value"))
+def select_value(value):
+    global country
+    country = value
+    return CountryInfo(value).geo_json()
 
 if __name__ == '__main__':
     app.run(debug=True)
